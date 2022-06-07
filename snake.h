d8f53b9c45e78ac992d2b649d50ce3f863079a4f
neilsuyukai <93537464+neilsuyukai@users.noreply.github.com>
2022-06-07 15:35:28 +0800

    Update snake.h

--- snake.h
+++ snake.h
@@ -5,7 +5,15 @@ struct SNAKE
     bool IsSurvivor;            //當前結點是否存在（被畫）
     int coor_x;                 //節點橫座標
     int coor_y;                 //節點縱座標
+    SNAKE *link;                //指向下一個節點的指標
+    //建構函式
+    SNAKE(int x, int y, bool survivor = true,SNAKE *link = NULL) {
+        //初始化座標值,賦值方式為tail派生
+        coor_x = x;
+        coor_y = y;
+    }
 };
+
 class Snake
 {
 public:
@@ -22,9 +30,9 @@ public:
     bool IsDeath();                             //是否碰撞草叢，是蛇死亡返回true，否則返回false
 protected:
     int len;                                    //蛇身長度_以塊為單位
-    Snake *snakeHead;                           //蛇頭指標
-    Snake *tail;                                //蛇尾指標
-    Snake *beforeTail;                          //尾巴節點的前一個節點，方便移動
+    SNAKE *snakeHead;                           //蛇頭指標
+    SNAKE *tail;                                //蛇尾指標
+    SNAKE *beforeTail;                          //尾巴節點的前一個節點，方便移動
     int directions;
 };
 #endif
